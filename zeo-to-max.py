eed50583e8b741068bf51f430962d5935809ebb0
Stig <stig@stig.io>
2018-08-21 22:49:25 +0200

    Rewritten to take arguments

--- zeo-to-max.py
+++ zeo-to-max.py
@@ -1,4 +1,6 @@
 # Imports
+import argparse
+from logging import info
 from ZeoRawData.BaseLink import BaseLink
 from ZeoRawData.Parser import Parser
 
@@ -7,23 +9,22 @@ from pythonosc import udp_client
 
 import json
 
-# Initialize
-link = BaseLink("/dev/tty.usbserial-FTHBZZ4L")
-parser = Parser()
+parser = argparse.ArgumentParser()
+parser.add_argument("--zeo-serial", required=True, help="Serial Port for the ZEO device")
+parser.add_argument("--osc-dest-port", required=True, help="UDP Port to send OSC messages to")
+parser.add_argument("--osc-dest-host", required=True, help="IP/host address to send OSC messages to")
 
-# Add the Parser to the BaseLink's callback
-link.addCallback(parser.update)
-client = udp_client.SimpleUDPClient("127.0.0.1", 13000)
+args = parser.parse_args()
 
-# Add your callback functions
 
-#def linkCallbackDebug(timestamp, timestamp_subsec, version, data):
-#    print("timestamp {0}, subsec {1}, version {2}, data {3}\n".format(timestamp, timestamp_subsec, version, data))
 
-#link.addCallback(linkCallbackDebug)
+link = BaseLink(args.zeo_serial)
+zeoparser = Parser()
+
+link.addCallback(zeoparser.update)
+client = udp_client.SimpleUDPClient(args.osc_dest_host, int(args.osc_dest_port))
 
 def sendToOSC(s):
-    print(s)
     if not s:
         return False
     for k in ['ZeoTimestamp',  'Impedance', 'SQI', 'Version', 'Waveform', 'SleepStage']:
@@ -36,14 +37,18 @@ def sendToOSC(s):
     if 'BadSignal' in s:
         client.send_message("/BadSignal", s['BadSignal'] or False)
 
-            
+### zeoparser.addEventCallback( ... )
+zeoparser.addSliceCallback(sendToOSC)
 
 
-#parser.addEventCallback(sendToOSC)
-parser.addSliceCallback(sendToOSC)
 
-# Start link
-link.start()
-while 1:
-    pass
+
+try:
+    link.start()
+    while 1:
+        pass
+except (KeyboardInterrupt, SystemExit):
+    info("Quitting...")
+finally:
+    sys.exit()
 
